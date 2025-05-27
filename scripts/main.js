5bed063bbb00fc3a73a87736418c0301f2c5ac5b
Rue87 <mrunya87@gmail.com>
2025-05-27 23:19:04 +0200

    Show centered loading message with delay during task fetch

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,23 @@ import {
   setupNewTaskModalHandler,
 } from "./ui/modalHandlers.js";
 
+let loadingMessageElement;
+
+ function showLoading() {
+  if(loadingMessageElement) {
+  loadingMessageElement.style.display = "block";
+  }
+}
+  function hideLoading() {
+    if (loadingMessageElement) {
+      loadingMessageElement.style.display = "none";
+    }
+  }
+
+/**
+ * Fetches tasks from the API.
+ * @returns {Promise<Array>} A promise that resolves to an array of tasks, or an empty array on error.
+ */
 async function fetchTasksFromAPI() {
   try {
     const response = await fetch("https://jsl-kanban-api.vercel.app");
@@ -17,11 +34,32 @@ async function fetchTasksFromAPI() {
 }
 
 async function initTaskBoard() {
+
+  showLoading();
+
+  try {
+  // Wait for tasks to be fetched before hiding loading message
   const tasks = await fetchTasksFromAPI();
+
   clearExistingTasks();
   renderTasks(tasks);
+
+  //Add a delay before hiding
+  setTimeout(() => {
+    hideLoading();
+  }, 5000); 
+  }catch (error) {
+    console.log("Error during task board initialization:", error);
+  } finally {  hideLoading();
+  }
+
+
   setupModalCloseHandler();
   setupNewTaskModalHandler();
 }
+document.addEventListener("DOMContentLoaded", () => {
+  loadingMessageElement = document.getElementById("loading-message");
+  initTaskBoard();
+});
 
-document.addEventListener("DOMContentLoaded", initTaskBoard);
+
